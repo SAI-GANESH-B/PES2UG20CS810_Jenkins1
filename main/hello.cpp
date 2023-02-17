6641ee7ab3950ee0570f5776d63aea244d883c73
Sai ganesh B <saiganeshb3@gmail.com>
2023-02-17 21:53:26 +0530

    thisishypersai

--- main/hello.cpp
+++ main/hello.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main(void) {
   cout << "Hello, this is hypers ai\n";
-  cout << "Hello, Jenkins\n";
+  cout << "Hello, balaya\n";
   cout << "I have successfully built and run\n";
   return 0;
 }
